d5bdd4d1aee291cd64eeced6f24c785901284269
Dmitriy <6lackswitch@gmail.com>
2024-02-07 09:29:09 +0300

    feat: added config

--- src/main/java/org/ui/common/Driver.java
+++ src/main/java/org/ui/common/Driver.java
@@ -3,18 +3,30 @@ package org.ui.common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class Driver {
     private static WebDriver driver;
+
     public static WebDriver getDriver() {
         if (driver != null) {
             return driver;
+        }
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/" +
+                "org/ui/config/config.properties")) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         System.setProperty("webdriver.chrome.driver", Driver.class.getResource("/chromedriver.exe").getPath());
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get(properties.getProperty("url"));
         return driver;
     }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,8 @@
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 
 import static org.ui.common.Driver.getDriver;
 
 public class BaseTest {
-    @BeforeSuite
-    public void openPage() {
-        getDriver().get("https://demoqa.com/automation-practice-form");
-    }
-
     @AfterSuite
     public void closePage() {
         getDriver().close();
